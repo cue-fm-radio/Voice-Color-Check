7a15969a921c8e09602e5b1b26e87b5a98777b18
yukiya-MacBookAir <s9wrwpiq7z3wknhxa7y2@docomo.ne.jp>
2025-12-05 20:17:40 +0900

    Implement code changes to enhance functionality and improve performance

--- App.tsx
+++ App.tsx
@@ -82,7 +82,7 @@ const App: React.FC = () => {
             {/* Logo Image */}
             <div className="w-28 h-28 md:w-32 md:h-32 relative flex-shrink-0 bg-white rounded-full p-1">
                <img 
-                 src="./logo.png" 
+                 src="/logo.png" 
                  alt="エンカレ！ロゴ" 
                  className="w-full h-full object-contain rounded-full"
                  onError={(e) => {
